de4ab252d32b4c2792ec4c663f0d9ab0cb92e144
Moshe Baram <{ID}+{username}@users.noreply.github.com>
2020-10-12 07:58:15 +0200

    Added some more code comments.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,14 @@ import "./App.css";
 import { AppContext } from "./AppContext";
 import AppRouter from "./AppRouter";
 
+// As making 10 requests did not make sense, I have made the request that gives all that data including data that is not relevant,
+// since it was a single, small one (33kb), so that works better.
+
+// Since the information never changes, I require the data only one time, and afterwards it is saved in the state.
+
+// driverStandings used by Champions.jsx
+// yearlyResult used by Details.jsx
+
 const App = () => {
   const [driverStandings, setDriverStandings] = useState(null);
   const [yearlyResults, setYearlyResults] = useState({});
